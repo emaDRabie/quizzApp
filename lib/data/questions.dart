351062792b351acc326a956e48e88a644ce011b0
emaDRabie <emadrabie046@gmail.com>
2024-09-29 19:25:55 +0300

    Upload the app

---
+++ lib/data/questions.dart
@@ -0,0 +1,10 @@
+import '../Models/quiz_question.dart';
+
+List<QuizQuestion> questions = [
+  QuizQuestion("What is the capital of India?",
+      ["Delhi", "Mumbai", "Chennai", "Kolkata"]),
+  QuizQuestion("what is the capital of France?",
+      ["London", "Paris", "New York", "Berlin" ]),
+  QuizQuestion("what is the capital of England?",
+      ["London", "Paris", "New York", "Berlin"]),
+];
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+// import 'package:quiz_app/Views/last_screen.dart';
+
+import 'Views/quiz.dart';
+
+void main() {
+  return runApp(const Quiz());
+}
